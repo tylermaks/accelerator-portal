24f0e9a35f88ae935d903196a5d2fbf88c4d3ba7
Tyler Maksymiw <tmaksymiw@MacBook-Air.local>
2024-12-20 14:48:13 -0500

    Update config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
     experimental: {
         serverActions: {
           allowedOrigins: [process.env.URL_ROOT],
