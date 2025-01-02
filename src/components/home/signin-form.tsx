006023d50a756d19e095d98eb3f9d431a4416d4b
Tyler Maksymiw <tmaksymiw@MacBook-Air.local>
2025-01-02 12:57:02 -0500

    Update signin form

--- src/components/home/signin-form.tsx
+++ src/components/home/signin-form.tsx
@@ -7,66 +7,54 @@ import PasswordInput from "../ui/password-input";
 import MainButton from "../ui/main-button";
 import LoadingSpinner from "../ui/loading-spinner";
 
-
 export default function SignInForm() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        setIsLoading(true)
+        event.preventDefault();
+        setIsLoading(true);
 
-        try{
-            const formData = new FormData(event.currentTarget)
+        try {
+            const formData = new FormData(event.currentTarget);
             const result = await login(formData);
-
-            console.log("RESULT FROM SIGNIN", result)
 
             if (result?.status === 400) {
                 setError("Invalid email or password");
-                setIsLoading(false)
+                setIsLoading(false);
             }
-        } catch(error) { 
-            setError("A server error occurred, please try again")
-            setIsLoading(false)
-            console.error(error)
-        } 
-    }
-      
+        } catch (error) {
+            setError("A server error occurred, please try again");
+            setIsLoading(false);
+            console.error(error);
+        }
+    };
 
     useEffect(() => {
-        setError("")
-    }, [password, email])
+        setError("");
+    }, [password, email]);
 
     return (
         <>
             <div className="text-center">
                 <h1 className="text-3xl text-gray-700 mb-2">Welcome back</h1>
                 <p className="text-gray-700 text-sm">Please enter your details to sign in</p>
-            </div>  
-        
-            <form onSubmit={handleLogin} className="flex flex-col gap-4 w-full">   
-                <Input 
+            </div>
+
+            <form onSubmit={handleLogin} className="flex flex-col gap-4 w-full">
+                <Input
                     label="Email:"
-                    type="email" 
+                    type="email"
                     id="email"
                     name="email"
                     isRequired={true}
                     setFormState={setEmail}
+                    prepopulate={email} // Added prepopulate prop
                 />
                 <>
-                    {/* <Input 
-                        label="Password:"
-                        type="password" 
-                        id="password"
-                        name="password"
-                        isRequired={true}
-                        setFormState={setPassword}
-                    /> */}
-
-                    <PasswordInput 
+                    <PasswordInput
                         label="Password:"
                         id="password"
                         name="password"
@@ -74,14 +62,14 @@ export default function SignInForm() {
                     />
                     {error && <p className="text-red-500 text-sm">{error}</p>}
                 </>
-               
-                <MainButton 
+
+                <MainButton
                     loading={isLoading}
                     id='signin'
                     text="Sign In"
-                />      
+                />
             </form>
-            { isLoading && <LoadingSpinner />}
+            {isLoading && <LoadingSpinner />}
         </>
     );
 }
