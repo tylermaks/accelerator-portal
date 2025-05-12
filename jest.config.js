f6b83c2c699a5a54d694a726da2ac4d7daeb2822
Tyler Maksymiw <tylermcodes@gmail.com>
2025-05-11 21:44:23 -0400

    test: Add test to catch server-only env variables in client components

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-
+  testEnvironment: 'jsdom',
+  testMatch: ['**/*.test.ts', '**/*.test.tsx'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
 }; 
