781f12cafa70539de8308fadbe7bfd26c3b87bd6
Tyler Maksymiw <tylermcodes@gmail.com>
2025-05-15 13:17:34 -0400

    chore: update yml and playwright config for deploy preview testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -67,9 +67,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-  },
+  webServer: process.env.PLAYWRIGHT_TEST_BASE_URL || process.env.PLAYWRIGHT_TEST_BASE_URL
+         ? undefined
+         : {
+             command: 'npm run dev',
+             url: 'http://localhost:3000',
+             reuseExistingServer: !process.env.CI,
+           },
 });
