a868b02fec630a4349cd8a63d9d62163ed4beeeb
Tyler Maksymiw <tylermcodes@gmail.com>
2025-05-14 22:00:36 -0400

    fix: Update playwright config for Netlify deployment

--- playwright.config.ts
+++ playwright.config.ts
@@ -68,8 +68,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:3000',
+    command: 'npm run start',
+    url: process.env.DEPLOY_URL || 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
   },
 });
