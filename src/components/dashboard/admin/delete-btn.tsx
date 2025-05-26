489a52ec29fc2edb40ba848c9366ad2b219c2e92
Tyler Maksymiw <tylermcodes@gmail.com>
2025-05-26 19:12:17 -0400

    test: add e2e test for create and delete user flow

--- src/components/dashboard/admin/delete-btn.tsx
+++ src/components/dashboard/admin/delete-btn.tsx
@@ -7,8 +7,18 @@ interface UserDeleteButtonProps {
 export default async function UserDeleteButton({ userID }: UserDeleteButtonProps) {
     return (
         <form action={deleteUser}>
-            <input type="text" id="userID" name="userID" className="hidden" defaultValue={userID}/>
-            <button className="rounded-md bg-red-500 p-1.5 text-white"> 
+            <input 
+                type="text" 
+                id="userID" 
+                name="userID" 
+                className="hidden" 
+                defaultValue={userID}
+            />
+            <button 
+                id="delete-user-btn"
+                data-btn-id={userID}
+                className="rounded-md bg-red-500 p-1.5 text-white"
+            > 
                 Delete User
             </button>
         </form>
